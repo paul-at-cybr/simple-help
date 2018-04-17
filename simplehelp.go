f82b447e4f66e1cde568325b9a509bfeb4ec8804
Paul Stølen <paul@dagbladet.no>
2018-04-17 16:21:41 +0200

    Added AddSection()
    - Adds a title/content section to the help output

--- simplehelp.go
+++ simplehelp.go
@@ -12,7 +12,13 @@ type SimpleHelp struct {
 	ProgramTitle       string // Title of your CLI program goes here
 	ProgramDescription string // Program description
 	Indentation        int    // How many characters to indent the flag descriptions
+	helpSections       []helpSection
 	helpFormatString   string
+}
+
+type helpSection struct {
+	name        string
+	description string
 }
 
 // Help - Gets available flags, combines with the config options,
@@ -27,6 +33,11 @@ func (h *SimpleHelp) Help() {
 	grebo.Printf("\n%s\n", h.ProgramTitle)
 	fmt.Println("  " + h.ProgramDescription)
 
+	for _, section := range h.helpSections {
+		grebo.Printf("\n%s\n", section.name)
+		fmt.Println("  " + section.description)
+	}
+
 	grebo.Print("\nFlags:\n")
 	// print usage for all flags
 	flag.CommandLine.VisitAll(func(fl *flag.Flag) {
@@ -34,6 +45,11 @@ func (h *SimpleHelp) Help() {
 	})
 
 	fmt.Print("\n")
+}
+
+// AddSection - Adds a help section to the help output.
+func (h *SimpleHelp) AddSection(name string, description string) {
+	h.helpSections = append(h.helpSections, helpSection{name: name, description: description})
 }
 
 // creates a formatting string that can be used for space-padding via Sprintf
@@ -46,7 +62,7 @@ func (h *SimpleHelp) flagIndentation(flagName string) string {
 	return fmt.Sprintf(h.helpFormatString, flagName)
 }
 
-// Hint: Prints a small hint about the --help parameter
+// Hint - Prints a small hint about the --help parameter
 func Hint() {
 	fmt.Println("Use --help for more information")
 }
